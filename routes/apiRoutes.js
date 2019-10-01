7e6036813859bf87ba6f886540f1e0a877879aab
Skyler Stevens <sabrinastevens@Administrators-MacBook-Pro.local>
2019-10-01 15:34:48 -0400

    Add delete and post api routes for contacts

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,45 +1,152 @@
-const axios = require('axios')
-const mysql = require('mysql')
-const router = require('express').Router()
+const mysql = require("mysql"); //Import mysql
+const router = require("express").Router();
 
+//Create a connection with the mySQL databse
 var connection = mysql.createConnection({
-    //host name
-    host: "localhost",
-  
-    //Port # default is 3306
-    port: 3306,
-  
-    // MySQL username
-    user: "root",
-  
-    // MySQL password
-    password: process.env.MYPASSWORD,
-    database: "hummingbird_guitars_db"
+  //Host name
+  host: "localhost",
+
+  //Port number default is 3306
+  port: 3306,
+
+  // MySQL username
+  user: "root",
+
+  // MySQL password saved in another file for security reasons
+  password: process.env.MYPASSWORD,
+
+  //Databse name
+  database: "hummingbird_guitars_db"
 });
-//Connect to database
+
+//Connect to database and print dB connected if succesful otherwise throw an error
 connection.connect(function(err) {
-    if (err) throw err;
-    console.log("dB connected")
-})
+  if (err) throw err;
+  console.log("dB connected");
+});
 
-router.get('/products', (req, res) => {
-    connection.query("SELECT products.product_id, brands.brand_name, products.product_name, products.product_details, products.img_url, products.img_alt, products.guitar_type, prices.price FROM products JOIN brands ON products.brand_id = brands.brand_id JOIN prices ON products.price_id = prices.price_id ORDER BY products.product_id;", (err, result) => {
-        if(err) console.log(err);
-        res.send(result)
-    })
-})
+//Execute a query to get all the info needed from the tables to have brand and price included with product info
+//http://localhost:3000/api/products
+router.get("/products", (req, res) => {
+  connection.query(
+    "SELECT products.product_id, brands.brand_name, products.product_name, products.product_details, products.img_url, products.img_alt, products.guitar_type, prices.price FROM products JOIN brands ON products.brand_id = brands.brand_id JOIN prices ON products.price_id = prices.price_id ORDER BY products.product_id;",
+    (err, result) => {
+      if (err) console.log(err);
+      res.send(result);
+    }
+  );
+});
 
-router.get('/contacts', (req, res) => {
-    connection.query("SELECT * FROM contacts", (err, result) => {
-        if(err) console.log(err);
-        res.send(result)
-    })
-})
+//Execute a query to get all the data from the contacts table
+//http://localhost:3000/contacts
+router.get("/contacts", (req, res) => {
+  connection.query(
+    "SELECT * FROM contacts ORDER BY contact_id DESC",
+    (err, result) => {
+      if (err) console.log(err);
+      res.send(result);
+    }
+  );
+});
 
-app.get('/api/productfilter/:query', (req, res) => {
-    const qryStr = req.params.query
-    connection.query(qryStr, (err, result) => {
+//http://localhost:3000/api/products/filter/electric/fender/900/1000
+router.get("/products/filter/:type/:brand/:pricelow/:pricehigh", (req, res) => {
+  const type = req.params.type; //define req.params.type passed through url as type
+  const brand = req.params.brand; //define req.params.brand passed through url as brand
+  const priceLow = req.params.pricelow; //define req.params.pricelow passed through url as priceLow
+  const priceHigh = req.params.pricehigh; //define req.params.pricehigh passed through url as priceHigh
+  const ANY = "any"; // define ANY for easy reassignment if that value gets changed on the page
+  let paramArr = []; //Create a redefinable variable paramArr with an empty array to be able to add to below with the values of the req.params
+  let qryStr = `SELECT products.product_id, brands.brand_name AS brand, products.product_name, products.product_details, products.img_url, products.img_alt, products.guitar_type AS type, prices.price AS price FROM products JOIN brands ON products.brand_id = brands.brand_id JOIN prices ON products.price_id = prices.price_id `; //Query string that will be added to based on the conditions below and then passed in the connection.query
+
+  if (brand !== ANY) {
+    //If brand does not equal ANY check if the paramArr array has anything in it if it is empty define qryStr a string with WHERE otherwise define it as  a string with &&
+    if (paramArr.length < 0) {
+      qryStr = `${qryStr} WHERE  brands.brand_name = ?`;
+    } else {
+      qryStr = `${qryStr} && brands.brand_name = ?`;
+    }
+    paramArr = [...paramArr, brand]; //Spread operator to redefine paramArr with what was already in the array and add brand
+  }
+  if (type !== ANY) {
+    if (paramArr.length < 0) {
+      qryStr = `${qryStr} WHERE products.guitar_type = ?`;
+    } else {
+      qryStr = `${qryStr} && products.guitar_type = ?`;
+    }
+    paramArr = [...paramArr, type]; //Add type to array
+  }
+  if (priceLow !== ANY) {
+    if (paramArr.length < 0) {
+      qryStr = `${qryStr} WHERE price BETWEEN ? AND ?`;
+    } else {
+      qryStr = `${qryStr} && price BETWEEN ? AND ?`;
+    }
+    paramArr = [...paramArr, priceLow, priceHigh]; //Add priceLow and priceHigh to array
+  }
+  qryStr = `${qryStr} ORDER BY products.product_id`;
+  //Connect to mySQL database and send the qryStr passing in the paramArr for what to filter for and send the result as a response
+  connection.query(qryStr, paramArr, (err, result) => {
+    if (err) console.log(err);
+    res.send(result);
+  });
+});
+
+router.post("/newcontact/:first/:last/:email/:comment", (req, res) => {
+  const firstName = req.params.first;
+  const lastName = req.params.last;
+  const email = req.params.email;
+  const comment = req.params.comment;
+  //Validate all the fields that the client should include in the new contact
+  if (
+    !firstName ||
+    firstName.length < 2 ||
+    firstName.length > 50 ||
+    !lastName ||
+    lastName.length < 2 ||
+    lastName.length > 50 ||
+    !email ||
+    email.length < 5 ||
+    email.length > 100 ||
+    !comment ||
+    comment.length < 5 ||
+    comment.length > 255
+  ) {
+    return res
+      .status(400)
+      .send(
+        "You must enter a first name, last name, email and comment. Names must be at least 2 characters, email and comments must be at least 5 characters long. Names cannot be longer than 50 characters, emails cannot be longer than 100 characters and comments cannot be longer than 255 characters."
+      );
+  } else {
+    connection.query(
+      `INSERT INTO contacts (firstname, lastname, email_address, user_message)
+      VALUES("${firstName}", "${lastName}", "${email}", "${comment}")`,
+      (err, response) => {
+        if (err) console.log(err);
+        connection.query(
+          "SELECT * FROM contacts ORDER BY contact_id DESC",
+          (err, result) => {
+            if (err) console.log(err);
+            res.send(result);
+          }
+        );
+      }
+    );
+  }
+});
+
+router.delete("/deletecontact/:id", (req, res) => {
+    const id = req.params.id
+  connection.query(
+    `DELETE FROM contacts WHERE contact_id = ${id}`,
+    (err, response) => {
+      if (err) console.log(err);
+      connection.query("SELECT * FROM contacts ORDER BY contact_id DESC", (err, result) => {
         if(err) console.log(err);
-        res.send(result)
-    })
-})
+        res.send(result);
+      });
+    }
+  );
+});
+
+module.exports = router;
