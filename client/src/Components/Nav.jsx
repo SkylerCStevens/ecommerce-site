2d9b14091257ce13bf40f1e7a10d79e792d3ae4a
Skyler Stevens <sabrinastevens@Administrators-MacBook-Pro.local>
2019-10-02 09:33:45 -0400

    Added bootstrap classes to float nav buttons to the right

--- client/src/Components/Nav.jsx
+++ client/src/Components/Nav.jsx
@@ -9,6 +9,14 @@ const Nav = () => {
     //To use react-router-dom you have to wrap the code for the links in <BrowserRouter>
     <BrowserRouter>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+      <img
+          src="https://static.vecteezy.com/system/resources/previews/000/344/259/non_2x/hummingbird-logo-illustration-of-a-bird-species-violetears-colibri-flat-vector-drawing-of-an-animal-fly.jpg"
+          alt="Hummingbird Logo"
+          className="logo nav-logo"
+        ></img>
+      <span className="navbar-brand mb-1 h1 nav-title">
+          Hummingbird Guitars
+        </span>
         <button
           className="navbar-toggler"
           type="button"
@@ -21,7 +29,7 @@ const Nav = () => {
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="nav navbar-nav navbar-right">
+          <ul className="nav navbar-nav navbar-right ml-auto">
             <li className="nav-item">
               {/* Using <NavLink> to be able to target active for a more user friendly active page different color button */}
               <NavLink exact className="btn text-white" to="/">
@@ -48,14 +56,6 @@ const Nav = () => {
             </li>
           </ul>
         </div>
-        <span className="navbar-brand mb-1 h1 nav-title">
-          Hummingbird Guitars
-        </span>
-        <img
-          src="https://static.vecteezy.com/system/resources/previews/000/344/259/non_2x/hummingbird-logo-illustration-of-a-bird-species-violetears-colibri-flat-vector-drawing-of-an-animal-fly.jpg"
-          alt="Hummingbird Logo"
-          className="logo nav-logo"
-        ></img>
       </nav>
       {/* This is where the routing paths are determined */}
       <Switch>
