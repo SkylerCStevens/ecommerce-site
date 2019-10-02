5eda83f6fb3b5ef4787facfdd83dcab040de3d37
Skyler Stevens <sabrinastevens@Administrators-MacBook-Pro.local>
2019-10-02 16:57:59 -0400

    Add automated testing

---
+++ test/test.js
@@ -0,0 +1,39 @@
+  
+const expect = require('chai').expect
+const request = require('request')
+
+describe('home page', () => {
+    it('should have a status code of 200', (done) => {
+        request('http://localhost:3000', (err, res, body) => {
+            expect(res.statusCode).to.equal(200)
+            done()
+        })
+    })
+})
+
+describe('Products', () => {
+    it('should return an object', (done) => {
+        request('http://localhost:3000/api/products', (err, res, body) => {
+            expect(res).to.be.an('object')
+            done()
+        })
+    })
+})
+
+describe('Product filter', () => {
+    it('should return an object', (done) => {
+        request('http://localhost:3000/api/products/filter/electric/squier/0/200', (err, res, json) => {
+        expect(res).to.be.an('object')
+        done()
+    })
+    })
+})
+
+describe('Contacts', () => {
+    it('should return an object', (done) => {
+        request('http://localhost:3000/api/contacts', (err, res, json) => {
+            expect(res).to.be.an('object')
+            done()
+        })
+    })
+})
